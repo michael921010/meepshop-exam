d13fc9f40b8b032b71f69e233b254c12d91989e3
Michael Li <s1002573@gmail.com>
2024-11-01 09:46:05 +0800

    design router page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import Home from "@/views/Home";
-import Editor from "@/views/Editor";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Stack, CircularProgress } from "@mui/material";
+
+const Home = lazy(() => import("@/views/Home"));
+const Editor = lazy(() => import("@/views/Editor"));
 
 export default function App() {
-  // return <Home />;
-  return <Editor value="" />;
+  return (
+    <Router>
+      <Suspense
+        fallback={
+          <Stack width="100dvw" height="90dvh" alignItems="center" justifyContent="center">
+            <CircularProgress />
+          </Stack>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
